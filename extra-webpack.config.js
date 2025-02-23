b8f75314f329305a5974d91ddd2dff52405b214c
Hipolito Nunez <206755950@uniparks.com>
2025-02-22 19:29:22 -0500

    fix image file error during npm run test

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,33 +1,20 @@
-1;
-2;
-3;
-4;
-5;
-6;
-7;
-8;
-9;
-10;
-11;
-12;
-13;
-14;
-15;
-16;
-17;
 // ============================================================================
 //    Author: Kenneth Perkins
 //    Date:   Dec 14, 2021
 //    Taken From: http://programmingnotes.org/
 //    File:  extra-webpack.config.js
-//    Description: Configures an Asset Module file loader for images
+//    Description: Configures loaders for testing environment
 // ============================================================================
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpg|gif)$/i,
+        test: /\.js$|jsx/,
         type: 'asset'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: 'null-loader'
       }
     ]
   }
