448e5170dc2705903c4cee44220f3b002cb4ce55
Hipolito Nunez <206755950@uniparks.com>
2025-02-22 21:36:21 -0500

    Fix unit tests

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,20 +1,33 @@
+1;
+2;
+3;
+4;
+5;
+6;
+7;
+8;
+9;
+10;
+11;
+12;
+13;
+14;
+15;
+16;
+17;
 // ============================================================================
 //    Author: Kenneth Perkins
 //    Date:   Dec 14, 2021
 //    Taken From: http://programmingnotes.org/
 //    File:  extra-webpack.config.js
-//    Description: Configures loaders for testing environment
+//    Description: Configures an Asset Module file loader for images
 // ============================================================================
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$|jsx/,
+        test: /\.(png|jpg|gif)$/i,
         type: 'asset'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        use: 'null-loader'
       }
     ]
   }
